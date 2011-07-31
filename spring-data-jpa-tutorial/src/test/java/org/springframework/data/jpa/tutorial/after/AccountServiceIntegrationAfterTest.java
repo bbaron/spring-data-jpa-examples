f3f1299b26380ec7903ae0a09f942ae158cf8560
Bruce Baron <baron.br@gmail.com>
2011-07-31 14:40:46 -0400

    Completed refactoring of AccountService to AccountRepository

--- spring-data-jpa-tutorial/src/test/java/org/springframework/data/jpa/tutorial/after/AccountServiceIntegrationAfterTest.java
+++ spring-data-jpa-tutorial/src/test/java/org/springframework/data/jpa/tutorial/after/AccountServiceIntegrationAfterTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.tutorial.before.AccountService;
 import org.springframework.data.jpa.tutorial.before.CustomerService;
 import org.springframework.data.jpa.tutorial.core.AbstractTutorialTest;
 import org.springframework.data.jpa.tutorial.core.Account;
@@ -19,7 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 public class AccountServiceIntegrationAfterTest extends AbstractTutorialTest {
 
     @Autowired
-    AccountService accountService;
+    AccountRepository accountRepository;
 
     @Autowired
     CustomerService customerService;
@@ -28,11 +27,11 @@ public class AccountServiceIntegrationAfterTest extends AbstractTutorialTest {
     @Test
     public void testname() throws Exception {
 
-        assertNotNull("accountService should be wired", accountService);
+        assertNotNull("accountService should be wired", accountRepository);
         assertNotNull("customerService should be wired", customerService);
         Customer customer = customerService.findById(1L);
 
-        List<Account> accounts = accountService.findByCustomer(customer);
+        List<Account> accounts = accountRepository.findByCustomer(customer);
 
         assertFalse(accounts.isEmpty());
         assertThat(accounts.get(0).getCustomer(), is(customer));
